182b65d09392fd0719f6c97ff9f9e5c986750c2c
ZNOW0 <47673358+ZNOW0@users.noreply.github.com>
2020-05-18 09:53:34 +0200

    Tes_release

---
+++ tt.py
@@ -0,0 +1,24 @@
+import tweepy
+
+API_P = "hGsNwUHfulv96hHMkiea5dXLc"
+API_S = "Su3zxlOVVmo4JOYm5D7RJZmvDP5luuczKuWbZTWQZ9zHAxQ26Z"
+
+Access_P = "852541877702275072-zOCzsHAqjMFXJpYpYkaHZq4xaeeXD2J"
+Access_S= "TDTWjEfb9fP3C5x81GC5unOsVGLuLZPSFER817bkmE9U2"
+
+auth = tweepy.OAuthHandler(API_P, API_S)
+auth.set_access_token(Access_P, Access_S)
+api = tweepy.API(auth)
+
+def Tweet_timeline():
+    user_search = input("What user do you want to check: ")
+    user = api.user_timeline(user_search)
+    for tweet in user:
+        print(tweet.text)
+
+Choose = int(input("Press 1 to check someones profile, press 2 for nothing: "))
+if Choose == 1:
+    Tweet_timeline()
+if Choose == 2:
+    print("Fail")
+
